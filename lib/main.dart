e38207c9f0fceda0f9de3a417b30e378a79842c5
Liviu <Liviu0010@gmail.com>
2022-10-17 11:22:19 +0300

    Fixed bad certificate error.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:radar_meteo/pages/radar_page.dart';
 import 'package:radar_meteo/pages/selection_page.dart';
 import "package:radar_meteo/pages/splash_page.dart";
 
+class CustomHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    var client = super.createHttpClient(context);
+    client.badCertificateCallback = (cert, host, port) => host == "www.meteoromania.ro";
+    return client;
+  }
+}
+
 
 void main() {
+  HttpOverrides.global = CustomHttpOverrides();
   runApp(MaterialApp(
       initialRoute: "/splash_page",
     routes: {
